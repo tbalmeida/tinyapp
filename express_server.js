68dd415c9032c522ef5262e9e9ade7d02c257308
tbalmeida <tbalmeida@gmail.com>
2020-01-20 20:14:50 +0000

    task ended. Set/Fetch commented

--- express_server.js
+++ express_server.js
@@ -14,3 +14,20 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
+ 
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
